9d707ca733ef09225656f0d2c4d47d09b6c74b58
sebastianruizdev1 <sebastianruizdev@gmail.com>
2025-06-11 19:28:57 -0600

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# C
+Exercices the programming language C.  
+## Author
+- [@sebastianruizdev1](https://github.com/sebastianruizdev1)
---
+++ scanf.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main(){
+    int number;
+    printf("Por favor ingresa un numero: ");
+    scanf("%d", &number);
+    printf("El numero que usted imprimio es: %d\n", number);
+    return 0;
+}
